4c0c1f0bb84c87cbf88db080184f62046ff4c8dc
avyan8148 <avyan8148@gmail.com>
2026-05-02 20:15:01 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,12 +1,21 @@
+alert("JS START");
+
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 400;
+if (!canvas) {
+  alert("Canvas NOT found ❌");
+} else {
+  alert("Canvas found ✅");
 
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+  const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "white";
-ctx.font = "30px Arial";
-ctx.fillText("GAME IS WORKING 🎮", 200, 200);
+  canvas.width = 800;
+  canvas.height = 400;
+
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "white";
+  ctx.font = "30px Arial";
+  ctx.fillText("NOW IT WORKS 🎮", 200, 200);
+}
